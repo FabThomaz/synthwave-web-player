7a24baba34b3a53157daebd0dc010d90c1f32bea
Fab Thomaz <fabthomaz@live.com>
2025-05-17 22:41:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         {
             title: "80s Retro Synthwave",
             artist: "Unknown Artist",
-            src: "https://drive.google.com/file/d/11dlge5_FEqpsxRs4HMTYet_vip3GPOGG/view?usp=drive_link",
+            src: "https://audio.com/fabricio-mendes/audio/80s-retro-synthwave",
             cover: "https://drive.google.com/file/d/1konAT1FVwlPZLmWN9TmgojUDK-ZoAGp2/view?usp=drive_link"
         },
         {
